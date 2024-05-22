9610d318700ac2a8ad55e882fb4d65fef64c4686
Bo_ <phanlakhon.ort@gmail.com>
2024-05-22 23:03:17 +0700

    update CORS header

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.use(express.json());
+// app.use(express.json());
+app.use(function (_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 let items = require("./book.json");
 
