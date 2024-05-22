d4a1310ef455c7ab67c00ac59cfcb8c582a9d2b8
Bo_ <phanlakhon.ort@gmail.com>
2024-05-22 23:35:27 +0700

    update use express

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-// app.use(express.json());
+app.use(express.json());
 app.use(function (_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
